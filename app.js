d81dbab7cc009c1ee909f9c90fda6e3ee59026d9
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-10-15 17:59:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-require("dotenv").config()
-const { Client } = require("twitter-api-sdk")
+import * as dotenv from "dotenv"
+dotenv.config()
+
+import { Client } from "twitter-api-sdk"
+import chalk from "chalk"
 
 async function main() {
   const client = new Client(process.env.BEARER_TOKEN)
-  const stream = client.tweets.searchStream()
+  const stream = client.tweets.searchStream({
+    expansions: ["author_id"],
+  })
   for await (const tweet of stream) {
     if (tweet.data) {
-      console.log(`${tweet.data.text}\n\n`)
+      const username = tweet.includes.users[0].username
+      const text = tweet.data.text
+      console.log(`${username}: ${text}\n\n`)
     } else {
       console.log(tweet)
     }
