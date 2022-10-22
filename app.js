82a9a971c13155be125dc7b7431c1c507923672c
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-10-22 08:57:39 -0700

    Add support for error object

--- app.js
+++ app.js
@@ -14,17 +14,20 @@ async function main() {
   })
   try {
     for await (const res of stream) {
-      if (res.data) {
-        const username = res.includes.users[0].username
-        if (blocklist.includes(username)) continue
-        const { id, created_at, text } = res.data
-        const tweet = await db("tweets").insert(
-          { id, created_at, username, text },
-          ["username", "text"]
-        )
-        console.log(`${chalk.bgRed(tweet[0].username)}: ${tweet[0].text}\n\n`)
-      } else {
-        console.log(res)
+      switch (true) {
+        case "data" in res:
+          const username = res.includes.users[0].username
+          if (blocklist.includes(username)) continue
+          const { id, created_at, text } = res.data
+          const tweet = await db("tweets").insert(
+            { id, created_at, username, text },
+            ["username", "text"]
+          )
+          console.log(`${chalk.bgRed(tweet[0].username)}: ${tweet[0].text}\n\n`)
+          break
+        case "errors" in res:
+          console.log(res)
+          break
       }
     }
   } catch (error) {
