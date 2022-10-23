f50cec33f55882318a1628dd5a000b1d99840d14
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-10-23 08:47:50 -0700

    Separate data access layer

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import { Client } from "twitter-api-sdk"
-import { blocklist } from "./blocklist.js"
-import { db } from "./db.js"
-import { config } from "./config/index.js"
+import { blocklist } from "./utils/blocklist.js"
 import { log } from "./utils/log.js"
+import { DataAccessLayer } from "./utils/dataAccessLayer.js"
+import { config } from "./config/index.js"
 
 async function main() {
   const client = new Client(config.bearerToken)
@@ -14,14 +14,18 @@ async function main() {
     for await (const res of stream) {
       switch (true) {
         case "data" in res:
-          const username = res.includes.users[0].username
-          if (blocklist.includes(username)) continue
-          const { id, created_at, text } = res.data
-          const [tweet] = await db("tweets").insert(
-            { id, created_at, username, text },
-            ["username", "text"]
-          )
+          const context = {
+            username: res.includes.users[0].username,
+            id: res.data.id,
+            created_at: res.data.created_at,
+            text: res.data.text,
+          }
+
+          if (blocklist.includes(context.username)) continue
+          const DAL = new DataAccessLayer(context)
+          const [tweet] = await DAL.insert()
           log.text(tweet)
+
           break
         case "errors" in res:
           console.log(res)
